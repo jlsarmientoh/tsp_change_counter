96e84973e145f1aea4e70295d43a279bc0d2e65b
Jorge Sarmiento <jorge.sarmiento@prodigious.cr>
2014-03-19 20:42:55 -0500

    primera clase java

---
+++ source/change-counter/src/main/java/co/edu/uniandes/changecounter/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package co.edu.uniandes.changecounter;
+
+/**
+ * @author Jorge Sarmiento
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
